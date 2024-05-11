f6d8765fe83c9966117a89bc562ac377a8e089db
Ramanjot Singh <125717171+iamrommy@users.noreply.github.com>
2024-05-11 20:53:18 +0530

    Update App2.js

--- src/App2.js
+++ src/App2.js
@@ -13,31 +13,32 @@ const App = ()=>{
     
     const [progress, setProgress] = useState(0);
   
-    let country = "us";
+    const [country, setCountry] = useState('us');
     // const [country, setCountry] = useState("in");
 
     const ApiKey = process.env.REACT_APP_NEW_API;
 
     return (
-        
-        <Router>
-            <LoadingBar
-                color='#f11946'
-                height={3}
-                progress={progress}
-            />
-            <Navbar />
-            <Routes>
-                <Route exact path="/" element={<News3 setProgress={setProgress} key="general" country={country} category="general" apikey={ApiKey}/>} />
-                <Route exact path="/business" element={<News3 setProgress={setProgress} key="business" country={country} category="business"  apikey={ApiKey}/>} />
-                <Route exact path="/entertainment" element={<News3 setProgress={setProgress} key="entertainment" country={country} category="entertainment"  apikey={ApiKey}/>} />
-                <Route exact path="/health" element={<News3 setProgress={setProgress} key="health" country={country} category="health"  apikey={ApiKey}/>} />
-                <Route exact path="/science" element={<News3 setProgress={setProgress} key="science" country={country} category="science"  apikey={ApiKey}/>} />
-                <Route exact path="/sports" element={<News3 setProgress={setProgress} key="sports" country={country} category="sports"  apikey={ApiKey}/>} />
-                <Route exact path="/technology" element={<News3 setProgress={setProgress} key="technology" country={country} category="technology"  apikey={ApiKey}/>} />
-            </Routes>
-            <Footer/>
-        </Router>
+        <div className='min-w-[375px]'>
+            <Router>
+                <LoadingBar
+                    color='#f11946'
+                    height={3}
+                    progress={progress}
+                />
+                <Navbar setCountry={setCountry} country={country}/>
+                <Routes>
+                    <Route exact path="/" element={<News3 setProgress={setProgress} key="general" country={country} category="general" apikey={ApiKey}/>} />
+                    <Route exact path="/business" element={<News3 setProgress={setProgress} key="business" country={country} category="business"  apikey={ApiKey}/>} />
+                    <Route exact path="/entertainment" element={<News3 setProgress={setProgress} key="entertainment" country={country} category="entertainment"  apikey={ApiKey}/>} />
+                    <Route exact path="/health" element={<News3 setProgress={setProgress} key="health" country={country} category="health"  apikey={ApiKey}/>} />
+                    <Route exact path="/science" element={<News3 setProgress={setProgress} key="science" country={country} category="science"  apikey={ApiKey}/>} />
+                    <Route exact path="/sports" element={<News3 setProgress={setProgress} key="sports" country={country} category="sports"  apikey={ApiKey}/>} />
+                    <Route exact path="/technology" element={<News3 setProgress={setProgress} key="technology" country={country} category="technology"  apikey={ApiKey}/>} />
+                </Routes>
+                <Footer/>
+            </Router>
+        </div>
     )
 }
 
