e838f92118f0789d5b4bdf868fbaff46c82f0f37
Ramanjot Singh <125717171+iamrommy@users.noreply.github.com>
2024-06-13 19:01:49 +0530

    Update Footer.js

--- src/components2/Footer.js
+++ src/components2/Footer.js
@@ -5,7 +5,7 @@ export class Footer extends Component {
     return (
         <footer className="bg-black text-white py-1 fixed bottom-0 w-full">
             <div className="container mx-auto text-center">
-                <p>&copy; 2023 NewsMonkey All rights reserved.</p>
+                <p>&copy; 2024 NewsMonkey All rights reserved.</p>
             </div>
         </footer>
     )
