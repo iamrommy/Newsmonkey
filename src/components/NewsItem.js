e8e1fe635863cd0f07d39b240b588383ae30040f
Ramanjot Singh <ramanjotsingh8574@gmail.com>
2024-11-24 00:30:43 +0530

    fixed author

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -19,6 +19,7 @@ const NewsItem = (props) => {
             const isBookmarked = user.Bookmarks.some(bookmark => bookmark.title === title);
             setClicked(isBookmarked);
         }
+        console.log();
     }, [user, title]);
 
     const HandleBookmark = async () => {
@@ -47,7 +48,7 @@ const NewsItem = (props) => {
             <div className="p-5"> 
                 <h5 className="mb-2 text-lg font-bold tracking-tight text-gray-900">{title}</h5>
                 <p className="mb-3 font-normal text-gray-700"> {description} </p>
-                <p className="mb-3 font-normal text-gray-700"><small>By {author} on {new Date(date).toGMTString()}</small></p>
+                <p className="mb-3 font-normal text-gray-700"><small>On {new Date(date).toGMTString()}</small></p>
                 <div className="flex justify-between items-center">
                     <a href={newsUrl} target="_blank" rel="noreferrer" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-black rounded-lg hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300"> Read more <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10"> <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M1 5h12m0 0L9 1m4 4L9 9"/></svg>
                     </a>
